3cc348bdead6acce4d2cffb9f176a4b0992dfbe8
brian moore <datastx@gmail.com>
2023-04-29 18:39:33 -0400

    fix comments

--- app/main.py
+++ app/main.py
@@ -1,23 +1,22 @@
-
-
 from antlr4 import FileStream, CommonTokenStream
 from app.grammars.LookMLLexer import LookMLLexer
 from app.grammars.LookMLParser import LookMLParser
 
 
-def main(file_path):
-    # Read the test file
-    input_stream = FileStream(file_path)
+def main(file_path: str) -> None:
+    """Parse a LookML file and print the parse tree.
 
-    # Create a lexer and parser for the input
+    Args:
+        file_path (str): fully qualified path to a LookML file
+    """
+    
+    input_stream = FileStream(file_path)
+    
     lexer = LookMLLexer(input_stream)
     token_stream = CommonTokenStream(lexer)
     parser = LookMLParser(token_stream)
 
-    # Parse the test file starting from the top-level rule
     tree = parser.lookml_file()
-
-    # Print the parse tree (optional)
     print(tree.toStringTree(recog=parser))
 
 
